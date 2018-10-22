691fd087d1f5358457fe603b0e160b40e46f89ca
fuksinjo <fuks.fcb@gmail.com>
2018-10-22 10:58:57 +0200

    vezba9

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,12 +1,96 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="/vivify/public/">Vivify</a>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        
+        @guest
+            @else
+            <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="https://laravel.com/docs/5.7">Documentation</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://laracasts.com/">Laracasts</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://laravel-news.com/">News</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://nova.laravel.com/">Nova</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://forge.laravel.com/">Forge</a>
+          </li>
+        </ul>
+        @endguest
+        
+       
+        <ul class="navbar-nav ml-md-auto">
+           
+        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                @if (Route::has('register'))
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                @endif
+                            </li>
+                        @else
+                        <form class="form-inline">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search">
+          <button class="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            {{ Auth::user()->first_name}} {{ Auth::user()->last_name}}
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+            
+            <a class="dropdown-item" href="https://github.com/fuksinjo/vivify">Github</a>
+            <a class="dropdown-item" href="https://app.vivifyscrum.com/boards/35111/sprint-backlog/110264">Vivify Scrum</a>
+              
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
 
-<nav class="navbar navbar-expand-md navbar-dark bg-dark navbar-laravel">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+            </div>
+          </li>-->
+
+                        @endguest
+       
+        </ul>
+      </div>
+    </nav>
+
+<!--<nav class="navbar navbar-expand-md navbar-dark bg-dark navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Page Title') }}
                 </a>
+
+               
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                
                     <span class="navbar-toggler-icon"></span>
+                    
+                    
                 </button>
+
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Page 1</a></li>
+                    <li><a href="#">Page 2</a></li> 
+                    <li><a href="#">Page 3</a></li> 
+                </ul>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
@@ -16,6 +100,9 @@
 
 
                     <ul class="navbar-nav ml-auto">
+
+
+  </div>
 
                         @guest
                             <li class="nav-item">
@@ -36,6 +123,30 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
+                                        {{ __('Vivify Scrum') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Github') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -43,9 +154,32 @@
                                         @csrf
                                     </form>
                                 </div>
+
                             </li>
+
+                            <div class="dropdown show" >
+                                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" style="background-color: #353A40; border-style:none" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{ Auth::user()->first_name}} {{ Auth::user()->last_name}}
+                                </a>
+
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                    <a class="dropdown-item" href="https://github.com/fuksinjo/vivify">Github</a>
+                                    <a class="dropdown-item" href="https://app.vivifyscrum.com/boards/35111/sprint-backlog/110264">Vivify Scrum</a>
+                                </div>
+                            </div>
+
                         @endguest
                     </ul>
                 </div>
             </div>
         </nav>
+        -->
